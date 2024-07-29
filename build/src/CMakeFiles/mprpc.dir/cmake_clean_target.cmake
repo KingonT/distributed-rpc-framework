02b56617d3eb72995189b65d5c2c2d8709651e6d
king <wdnmd1935@outlook.com>
2024-07-29 17:53:53 +0800

    delete build/

--- build/src/CMakeFiles/mprpc.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "/home/king/PROJECT_Cpp/RPC-0.0.0/lib/libmprpc.a"
-)
--- build/src/CMakeFiles/mprpc.dir/mprpccontroller.cc.o.d
+++
@@ -1,3 +0,0 @@
-src/CMakeFiles/mprpc.dir/mprpccontroller.cc.o: \
- /home/king/PROJECT_Cpp/RPC-0.0.0/src/mprpccontroller.cc \
- /usr/include/stdc-predef.h
